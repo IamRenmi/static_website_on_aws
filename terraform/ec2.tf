ce2d34aefd19775e97d0d76982bff2628cd6a6cd
IamRenmi <renmi13@outlook.com>
2025-05-08 00:08:57 +0200

    launch wp server

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -12,3 +12,19 @@ resource "aws_instance" "bastion" {
     Name = "bastion"
   }
 }
+
+
+resource "aws_instance" "wp_server" {
+  ami                         = "ami-0f88e80871fd81e91"
+  instance_type               = "t2.micro"
+  key_name                    = "wp-key"
+  subnet_id                   = aws_subnet.public_a.id
+  vpc_security_group_ids      = [aws_security_group.wordpress_sg.id]
+  associate_public_ip_address = true
+
+  user_data = file("../user_data/userdata_initial_variable.sh")
+
+  tags = {
+    Name = "wp-server"
+  }
+}
