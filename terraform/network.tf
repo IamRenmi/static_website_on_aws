a8eed414641f5f4b0408fc8590a69a4c68b1d520
IamRenmi <renmi13@outlook.com>
2025-05-07 23:43:40 +0200

    add public route associaition

--- terraform/network.tf
+++ terraform/network.tf
@@ -68,3 +68,15 @@ resource "aws_route_table" "public_rtb" {
     Name = "public-rtb"
   }
 }
+
+# Associate public_a subnet with public route table
+resource "aws_route_table_association" "public_a_assoc" {
+  subnet_id      = aws_subnet.public_a.id
+  route_table_id = aws_route_table.public_rtb.id
+}
+
+# Associate public_b subnet with public route table
+resource "aws_route_table_association" "public_b_assoc" {
+  subnet_id      = aws_subnet.public_b.id
+  route_table_id = aws_route_table.public_rtb.id
+}
