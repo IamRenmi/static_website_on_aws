5f8afc229a7f4cd6dcf61a56280a343142fe4d51
IamRenmi <renmi13@outlook.com>
2025-05-08 21:38:18 +0200

    changed az for subnets

--- terraform/network.tf
+++ terraform/network.tf
@@ -8,7 +8,7 @@ resource "aws_vpc" "wordpress_vpc" {
 resource "aws_subnet" "public_a" {
   vpc_id            = aws_vpc.wordpress_vpc.id
   cidr_block        = "48.0.1.0/24"
-  availability_zone = "us-east-1a"
+  availability_zone = "eu-central-1a"
 
   tags = {
     Name = "public_a"
@@ -18,7 +18,7 @@ resource "aws_subnet" "public_a" {
 resource "aws_subnet" "private_a" {
   vpc_id            = aws_vpc.wordpress_vpc.id
   cidr_block        = "48.0.2.0/24"
-  availability_zone = "us-east-1a"
+  availability_zone = "eu-central-1a"
 
   tags = {
     Name = "private_a"
@@ -28,7 +28,7 @@ resource "aws_subnet" "private_a" {
 resource "aws_subnet" "public_b" {
   vpc_id            = aws_vpc.wordpress_vpc.id
   cidr_block        = "48.0.3.0/24"
-  availability_zone = "us-east-1b"
+  availability_zone = "eu-central-1b"
 
   tags = {
     Name = "public_b"
@@ -38,7 +38,7 @@ resource "aws_subnet" "public_b" {
 resource "aws_subnet" "private_b" {
   vpc_id            = aws_vpc.wordpress_vpc.id
   cidr_block        = "48.0.4.0/24"
-  availability_zone = "us-east-1b"
+  availability_zone = "eu-central-1b"
 
   tags = {
     Name = "private_b"
