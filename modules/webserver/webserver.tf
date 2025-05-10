88c381cd93a606e559abf8ce638ad9d866ebd702
IamRenmi <renmi13@outlook.com>
2025-05-10 12:36:38 +0200

    fix webserver count issue

--- modules/webserver/webserver.tf
+++ modules/webserver/webserver.tf
@@ -1,13 +1,13 @@
-resource "aws_instance" "web" {
-  ami                       = var.ami_id
-  instance_type             = var.instance_type
-  key_name                  = var.key_name
-  subnet_id                 = element([var.subnet_id, var.subnet_id], count.index)  # will be overridden per call
+resource "aws_instance" "webserver" {
+  ami                         = var.ami_id
+  instance_type               = var.instance_type
+  key_name                    = var.key_name
+  subnet_id                   = var.subnet_id
   associate_public_ip_address = false
-  vpc_security_group_ids    = [var.security_group_id]
+  vpc_security_group_ids      = [var.security_group_id]
 
   tags = {
-    Name = format("webserver-%s", count.index == 0 ? "a" : "b")
+    Name = var.key_name == "wordpress" && var.subnet_id != "" ? "webserver-${var.subnet_id}" : "webserver"
   }
 
   user_data = base64encode(<<-EOF
@@ -16,6 +16,7 @@ sudo yum update -y
 sudo yum install -y httpd httpd-tools mod_ssl
 sudo systemctl enable httpd
 sudo systemctl start httpd
+
 sudo amazon-linux-extras enable php7.4
 sudo yum clean metadata
 sudo yum install -y php php-common php-pear php-{cgi,curl,mbstring,gd,mysqlnd,gettext,json,xml,fpm,intl,zip}
@@ -27,7 +28,7 @@ sudo systemctl enable mysqld
 sudo systemctl start mysqld
 
 # Mount EFS via fstab
-echo "${var.efs_mount_dns}.efs.${var.region}.amazonaws.com:/ /var/www/html nfs4 nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2 0 0" | sudo tee -a /etc/fstab
+sudo echo "${var.efs_mount_dns}.efs.${var.region}.amazonaws.com:/ /var/www/html nfs4 nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2 0 0" | sudo tee -a /etc/fstab
 sudo mount -a
 
 sudo chown -R apache:apache /var/www/html
