fac666c4a95d693e9016d205d02fcf1f384be87e
IamRenmi <renmi13@outlook.com>
2025-05-10 21:32:17 +0200

    edit launch template

--- modules/launch_template/launch_template.tf
+++ modules/launch_template/launch_template.tf
@@ -6,36 +6,24 @@ resource "aws_launch_template" "wp" {
 
   user_data = base64encode(<<-EOF
 #!/bin/bash
-set -e
-
 yum update -y
-# Install HTTPD, SSL and start
-yum install -y httpd httpd-tools mod_ssl
-systemctl enable httpd
-systemctl start httpd
-
-# Install PHP 7.4 and extensions
-amazon-linux-extras enable php7.4
-yum clean metadata
-yum install -y php php-common php-pear \
-  php-{cgi,curl,mbstring,gd,mysqlnd,gettext,json,xml,fpm,intl,zip}
-
-# Install MySQL server
-rpm -Uvh https://dev.mysql.com/get/mysql57-community-release-el7-11.noarch.rpm
-rpm --import https://repo.mysql.com/RPM-GPG-KEY-mysql-2022
-yum install -y mysql-community-server
-systemctl enable mysqld
-systemctl start mysqld
-
-# Mount EFS via fstab
-echo "${var.efs_mount_dns}.efs.${var.region}.amazonaws.com:/ /var/www/html nfs4 \
-  nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2 0 0" | sudo tee -a /etc/fstab
+sudo yum install -y httpd httpd-tools mod_ssl
+sudo systemctl enable httpd 
+sudo systemctl start httpd
+sudo amazon-linux-extras enable php7.4
+sudo yum clean metadata
+sudo yum install php php-common php-pear -y
+sudo yum install php-{cgi,curl,mbstring,gd,mysqlnd,gettext,json,xml,fpm,intl,zip} -y
+sudo rpm -Uvh https://dev.mysql.com/get/mysql57-community-release-el7-11.noarch.rpm
+sudo rpm --import https://repo.mysql.com/RPM-GPG-KEY-mysql-2022
+sudo yum install mysql-community-server -y
+sudo systemctl enable mysqld
+sudo systemctl start mysqld
+echo "${var.efs_mount_dns}.efs.${var.region}.amazonaws.com:/ /var/www/html nfs4 nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2 0 0" >> /etc/fstab
 mount -a
-
-# Ensure apache owns web root
 chown apache:apache -R /var/www/html
+sudo service httpd restart
 
-echo "Launch template wp-template configured."
 EOF
   )
 }
