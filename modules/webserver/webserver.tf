8955337070d3e31efcd15c8f25b3d876a8b980a4
IamRenmi <renmi13@outlook.com>
2025-05-10 12:34:52 +0200

    reduce server count

--- modules/webserver/webserver.tf
+++ modules/webserver/webserver.tf
@@ -1,5 +1,4 @@
 resource "aws_instance" "web" {
-  count                     = 2
   ami                       = var.ami_id
   instance_type             = var.instance_type
   key_name                  = var.key_name
